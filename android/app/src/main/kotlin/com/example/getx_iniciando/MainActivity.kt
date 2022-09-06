4f7b8190c332ed22bce04c77d3e60971858c1354
leo <realleo2009@hotmail.com>
2022-09-05 23:10:27 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/getx_iniciando/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getx_iniciando
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    );
+  }
+}
